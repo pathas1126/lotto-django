1008f3b7a06950c138053f4163da7c44689d6345
pathas1126 <pathas@naver.com>
2020-11-03 14:52:23 +0900

    Initial Commit

---
+++ lotto_project/mysql_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lotto_django',
+        'USER': 'root',
+        'PASSWORD': '11111111',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
